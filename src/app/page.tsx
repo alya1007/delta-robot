aa095b2ab6a7ee7f01a49a3af38da0c41a7088a7
alya1007 <alexandra.konjevic@isa.utm.md>
2024-06-11 00:22:32 +0300

    feat(camera-preview): make preview show either video or placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
+"use client";
+
 import Logs from "@/components/logs/Logs";
 import Statistics from "@/components/Statistics";
-import Card from "@/components/ui/Card";
 import VideoCard from "@/components/video/VideoCard";
+import { useState } from "react";
 
 export default function Home() {
+	const [showVideo, setShowVideo] = useState<boolean>(true);
 	return (
 		<main className="text-center w-full p-10 max-h-screen">
 			<div className="flex w-full flex-col items-center justify-start gap-10">
 				<h1 className="text-3xl font-medium">Delta Sort</h1>
 				<div className="flex justify-end w-full gap-10">
-					<VideoCard src="/images/camera-placeholder.jpg" />
+					<VideoCard
+						src="/images/camera-placeholder.jpg"
+						videoSrc="/videos/output.mp4"
+						showVideo={showVideo}
+					/>
 					<Logs />
 				</div>
 				<Statistics />
--- src/components/video/VideoCard.tsx
+++ src/components/video/VideoCard.tsx
@@ -1,40 +1,28 @@
-"use client";
-
-import { useState } from "react";
-import { motion } from "framer-motion";
 import Image from "next/image";
-import VideoModal from "./VideoModal";
 
 type VideoCardProps = {
-	src?: string;
+	src: string;
+	videoSrc: string;
+	showVideo: boolean;
 };
 
-const VideoCard = ({ src }: VideoCardProps) => {
-	const [isOpen, setIsOpen] = useState<boolean>(false);
-
+const VideoCard = ({ src, videoSrc, showVideo }: VideoCardProps) => {
 	return (
-		<div>
-			<div className="group">
-				<motion.div className="relative rounded-4xl">
-					<motion.div className="bg-card-bg hover:opacity-30 transition-all duration-300 ease-in-out rounded-4xl">
-						<motion.img
-							src={src}
-							className="cursor-pointer rounded-4xl"
-							onClick={() => setIsOpen(true)}
-							layoutId="expandable-image"
-						/>
-					</motion.div>
-					<motion.div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 duration-300 ease-in-out">
-						<Image
-							src={"/images/camera-icon.png"}
-							alt="camera icon"
-							width={150}
-							height={150}
-						/>
-					</motion.div>
-				</motion.div>
-			</div>
-			{isOpen && <VideoModal src={src} onClose={() => setIsOpen(false)} />}
+		<div className="w-full">
+			{showVideo ? (
+				<div className="h-full">
+					<video src={videoSrc} className="rounded-4xl h-[380px]" controls />
+				</div>
+			) : (
+				<div className="relative h-full">
+					<Image
+						src={src}
+						alt={"placeholder image"}
+						className="rounded-4xl"
+						fill={true}
+					/>
+				</div>
+			)}
 		</div>
 	);
 };
